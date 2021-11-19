ad4312f409eee53b8f0ba45ccc26cb574355fe82
Anh-JUN-1801 <81857289+Anh-JUN-1801@users.noreply.github.com>
2021-11-19 20:02:46 +0700

    First

---
+++ GiaoDien.py
@@ -0,0 +1,124 @@
+import pygame
+from Define import *
+
+
+def initWindown():
+    menuScreen = pygame.display.set_mode((WIDTH, HEIGHT))
+    icon = pygame.image.load('icon/icon.png')
+    pygame.display.set_icon(icon)
+    pygame.display.set_caption('2048')
+    menuScreen.fill(BG_SCREEN)
+    return menuScreen
+
+
+def setMenuScreen(menuScreen):
+    menuScreen.fill(BG_SCREEN)
+    game = pygame.image.load('icon/Game_led1.png')
+    menuScreen.blit(game, (120, 0))
+
+    bg = pygame.image.load('icon/bg.png')
+    menuScreen.blit(bg, (120, 100))
+
+    pygame.draw.rect(menuScreen, BG_BLOCK, (WIDTH / 2 - 125, 300, 250, 50), border_radius=8)
+    font = pygame.font.SysFont('Bahnschrift', 32)
+    text = font.render('Play Game', True, (0, 0, 0))
+    menuScreen.blit(text, (WIDTH / 2 - 65, 302))
+
+    pygame.draw.rect(menuScreen, BG_BLOCK, (WIDTH / 2 - 125, 370, 250, 50), border_radius=8)
+    font = pygame.font.SysFont('Bahnschrift', 32)
+    text = font.render('Rank', True, (0, 0, 0))
+    menuScreen.blit(text, (WIDTH / 2 - 30, 375))
+
+    pygame.draw.rect(menuScreen, BG_BLOCK, (WIDTH / 2 - 125, 440, 250, 50), border_radius=8)
+    font = pygame.font.SysFont('Bahnschrift', 32)
+    text = font.render('Quit', True, (0, 0, 0))
+    menuScreen.blit(text, (WIDTH / 2 - 30, 445))
+
+
+def thanhcongcu(menuScreen: pygame.Surface):
+    pygame.draw.rect(menuScreen, BG_SCREEN, (0, 0, 530, 60), border_radius=8)
+    pygame.draw.rect(menuScreen, BG_BLOCK, (20, 10, 40, 40), border_radius=8)
+    home = pygame.image.load('icon/Home.png')
+    menuScreen.blit(home, (22, 12))
+
+    pygame.draw.rect(menuScreen, BG_BLOCK, (70, 10, 40, 40), border_radius=8)
+    home = pygame.image.load('icon/reset.png')
+    menuScreen.blit(home, (69, 10))
+
+    # khung in điểm hiện tại
+    pygame.draw.rect(menuScreen, BG_TEXT, (155, 10, 160, 40), border_radius=5)
+    font = pygame.font.SysFont('Bahnschrift', 15)
+    text = font.render('Score:', True, (0, 0, 0))
+    menuScreen.blit(text, (166, 18))
+
+    # khung in điểm cao nhất
+    pygame.draw.rect(menuScreen, BG_TEXT, (330, 10, 160, 40), border_radius=5)
+    font = pygame.font.SysFont('Bahnschrift', 15)
+    text = font.render('Best: ', True, (0, 0, 0))
+    menuScreen.blit(text, (341, 18))
+
+
+def background(menuScreen: pygame.Surface) -> None:
+    pygame.draw.rect(menuScreen, NEN, (0, 60, 530, 530))
+    for i in range(COLS):
+        for j in range(LINE):
+            rect = (i * 130 + 10, j * 130 + 70, 116, 116)
+            pygame.draw.rect(menuScreen, BG_BLOCK, rect, border_radius=8)
+
+
+def drawText(menuScreen: pygame.Surface, rect: pygame.Rect, val):
+    font = pygame.font.Font('assets/fonts/ClearSans-Bold.ttf', FRONT_SIZE[val])
+    text = font.render(str(val), True, (250, 250, 245))
+    if val >= 2048:
+        text = font.render(str(val), True, (255, 120, 20))
+    text_x = rect.center[0] - text.get_rect().width // 2
+    text_y = rect.center[1] - text.get_rect().height // 2 - 5
+    menuScreen.blit(text, (text_x, text_y))
+
+
+def drawBlock(menuScreen: pygame.Surface, ds) -> None:
+    for i in range(COLS):
+        for j in range(LINE):
+            val = ds[i][j]
+            if val != 0:
+                rect = pygame.Rect(j * 130 + 10, i * 130 + 70, 116, 116)
+                pygame.draw.rect(menuScreen, BLOCK_COLORS[val], rect, border_radius=8)
+                drawText(menuScreen, rect, val)
+
+
+def drawmh(menuScreen: pygame.Surface, ds):
+    background(menuScreen)
+    drawBlock(menuScreen, ds)
+
+
+def drawgameover(menuScreen: pygame.Surface):
+    menuScreen1 = menuScreen.convert_alpha()
+    menuScreen1.fill((255, 160, 122, 150))
+    font = pygame.font.Font('assets/fonts/ClearSans-Bold.ttf', 30)
+    text = font.render('Game  Over', True, (0, 0, 10))
+    menuScreen1.blit(text, (175, 200))
+    menuScreen.blit(menuScreen1, (0, 60))
+def bangxephang(menuScreen: pygame.Surface,) -> None:
+    menuScreen.fill((0,0,0))
+    pygame.draw.rect(menuScreen, (102, 205, 170), (100, 60, 330, 460))
+    pygame.draw.rect(menuScreen, (176, 224, 230), (180, 30, 170, 50), border_radius=8)
+    game = pygame.image.load('icon/top10.png')
+    menuScreen.blit(game, (200, 30))
+
+    pygame.draw.rect(menuScreen, BG_BLOCK, (20, 10, 40, 40), border_radius=8)
+    home = pygame.image.load('icon/Home.png')
+    menuScreen.blit(home, (22, 12))
+
+    file = open('bangxephang.txt')
+    list = file.readlines()
+    for i in range(len(list)):
+        if i< 3:
+            pygame.draw.rect(menuScreen, COLORBXH[i], (145, 90 + 43 * i, 240, 38), border_radius=10)
+            font = pygame.font.SysFont('Bahnschrift', 20)
+            text = font.render(list[i].rstrip(), True, (0, 0, 0))
+            menuScreen.blit(text, (250, 93 + 43 * i))
+        else:
+            pygame.draw.rect(menuScreen, (240,248,255),(145, 90 + 43*i,240,38), border_radius=10)
+            font = pygame.font.SysFont('Bahnschrift', 20)
+            text = font.render(list[i].rstrip(), True, (0, 0, 0))
+            menuScreen.blit(text, (250, 93 + 43*i))
---
+++ main.py
@@ -0,0 +1,335 @@
+import random
+
+import pygame.mixer
+
+from GiaoDien import *
+from Define import *
+
+
+def khoitao(ds):
+    for i in range(4):
+        for j in range(4):
+            ds[i][j] = 0
+    for k in range(2):
+        add_khoi(ds);
+
+
+def add_khoi(ds):
+    while True:
+        i = random.randint(0, 3)
+        j = random.randint(0, 3)
+        if (ds[i][j] == 0):
+            break
+
+    ds[i][j] = random.choice(VAL_CHOICE)
+
+
+def output_ds(ds):
+    print("======================")
+    for i in range(4):
+        for j in range(4):
+            print(format(ds[i][j], "3d"), end='    ')
+        print()
+
+
+def updatediem(plus):
+    global souce
+    souce += plus
+    pygame.draw.rect(menuScreen, BG_TEXT, (220, 10, 90, 40), )
+    font = pygame.font.SysFont('Bahnschrift', 15)
+    text = font.render(str(souce), True, (0, 0, 0))
+    menuScreen.blit(text, (220, 18))
+def getbest():
+    file = open('bangxephang.txt')
+    best=int(file.readline())
+    pygame.draw.rect(menuScreen, BG_TEXT, (400, 10, 90, 40), )
+    font = pygame.font.SysFont('Bahnschrift', 15)
+    text = font.render(str(best), True, (0, 0, 0))
+    menuScreen.blit(text, (400, 18))
+
+def dichtrai(ds):
+    dsdadichchuyen = []
+    codichchuyen = False
+    for i in range(4):
+        for j in range(1, 4):
+            if ds[i][j] == 0:
+                continue
+            for k in reversed(range(j)):
+                if (ds[i][k] != 0):
+                    x = i
+                    y = k
+                    break
+            else:
+                x = i
+                y = j
+            if (x, y) == (i, j):
+                ds[i][0] = ds[x][y]
+                ds[x][y] = 0
+                codichchuyen = True
+            else:
+                if ds[i][j] == ds[x][y] and (x, y) not in dsdadichchuyen:
+                    ds[x][y] *= 2;
+                    ds[i][j] = 0
+                    dsdadichchuyen.append((x, y))
+                    updatediem(ds[x][y])
+                    codichchuyen = True
+                elif (y + 1 != j):
+                    ds[x][y + 1] = ds[i][j]
+                    ds[i][j] = 0
+                    codichchuyen = True
+    return codichchuyen
+
+
+def dichphai(ds):
+    dsdadichchuyen = []
+    codichchuyen = False
+    for i in range(4):
+        for j in reversed(range(3)):
+            if ds[i][j] == 0:
+                continue
+            for k in range(j + 1, 4):
+                if (ds[i][k] != 0):
+                    x = i
+                    y = k
+                    break
+            else:
+                x = i
+                y = j
+            if (x, y) == (i, j):
+                ds[i][3] = ds[x][y]
+                ds[x][y] = 0
+                codichchuyen = True
+            else:
+                if ds[i][j] == ds[x][y] and (x, y) not in dsdadichchuyen:
+                    ds[x][y] *= 2;
+                    ds[i][j] = 0
+                    dsdadichchuyen.append((x, y))
+                    updatediem(ds[x][y])
+                    codichchuyen = True
+                elif (y - 1 != j):
+                    ds[x][y - 1] = ds[i][j]
+                    ds[i][j] = 0
+                    codichchuyen = True
+
+    return codichchuyen
+
+
+def dichlen(ds):
+    dsdadichchuyen = []
+    codichchuyen = False
+    for j in range(4):
+        for i in range(1, 4):
+            if ds[i][j] == 0:
+                continue
+            for k in reversed(range(i)):
+                if (ds[k][j] != 0):
+                    x = k
+                    y = j
+                    break
+            else:
+                x = i
+                y = j
+            if (x, y) == (i, j):
+                ds[0][j] = ds[x][y]
+                ds[x][y] = 0
+                codichchuyen = True
+            else:
+                if ds[i][j] == ds[x][y] and (x, y) not in dsdadichchuyen:
+                    ds[x][y] *= 2;
+                    ds[i][j] = 0
+                    dsdadichchuyen.append((x, y))
+                    updatediem(ds[x][y])
+                    codichchuyen = True
+                elif (x + 1 != i):
+                    ds[x + 1][j] = ds[i][j]
+                    ds[i][j] = 0
+                    codichchuyen = True
+    return codichchuyen
+
+
+def dichxuong(ds):
+    dsdadichchuyen = []
+    codichchuyen = False
+    for j in range(4):
+        for i in reversed(range(3)):
+            if ds[i][j] == 0:
+                continue
+            for k in range(i + 1, 4):
+                if (ds[k][j] != 0):
+                    x = k
+                    y = j
+                    break
+            else:
+                x = i
+                y = j
+            if (x, y) == (i, j):
+                ds[3][j] = ds[x][y]
+                ds[x][y] = 0
+
+                codichchuyen = True
+            else:
+                if ds[i][j] == ds[x][y] and (x, y) not in dsdadichchuyen:
+                    ds[x][y] *= 2;
+                    ds[i][j] = 0
+                    dsdadichchuyen.append((x, y))
+                    updatediem(ds[x][y])
+                    codichchuyen = True
+                elif (x - 1 != i):
+                    ds[x - 1][j] = ds[i][j]
+                    ds[i][j] = 0
+                    codichchuyen = True
+    return codichchuyen
+
+
+def khongTheDiChuyen(ds):
+    for i in range(4):
+        for j in range(4):
+            if ds[i][j] == 0:
+                return False
+            if i > 0:
+                if ds[i - 1][j] == ds[i][j]:
+                    return False
+            if i < 3:
+                if ds[i + 1][j] == ds[i][j]:
+                    return False
+            if j > 0:
+                if ds[i][j - 1] == ds[i][j]:
+                    return False
+            if j < 3:
+                if ds[i][j + 1] == ds[i][j]:
+                    return False
+
+    return True
+
+
+def isFull(ds):
+    for i in range(4):
+        for j in range(4):
+            if (ds[i][j] == 0):
+                return False
+    return True
+
+
+def isWin(ds):
+    for i in range(4):
+        for j in range(4):
+            if (ds[i][j] == 2048):
+                return True
+    return False
+
+
+def updateBXH(souce):
+    files = open('bangxephang.txt')
+    list = files.readlines()
+    if (int(list[len(list) - 1]) >= souce):
+        return
+    files = open('bangxephang.txt', 'w')
+    list.append(str(souce) + '\n')
+    list = [int(i) for i in list]
+    list.sort(reverse=True)
+    if len(list) > 10:
+        del list[10:len(list)]
+    list = [str(i) + '\n' for i in list]
+    files.writelines(list)
+
+
+if __name__ == '__main__':
+    ds = [[0 for i in range(4)] for j in range(4)]
+    souce = 0
+    khoitao(ds)
+    #  ds = [[2,4,8,16],[32,64,128,256],[512,1024,1024,0],[4096,0,0,0]]
+    output_ds(ds)
+    pygame.init()
+    menuScreen = initWindown()
+    setMenuScreen(menuScreen)
+    pygame.display.update()
+    clock = pygame.time.Clock()
+    audio = pygame.mixer.Sound
+    musicHome = audio('assets/music/SoundTheme.mp3')
+    musicGameOver = audio('./assets/music/MusicGameOver.mp3')
+    musicWin = audio('./assets/music/MusicWin.mp3')
+    musicHome.play(10)
+
+    game_over = False
+    running = True
+    modes = {"Play": False,
+             "Home": True
+             }
+    win = False;
+    while running:
+        clock.tick(40)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                updateBXH(souce)
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if WIDTH / 2 - 125 <= mouse[0] <= (WIDTH / 2 - 125 + 250) and 300 <= mouse[1] <= 350 and modes[
+                    "Home"] == True:
+                    background(menuScreen)
+                    thanhcongcu(menuScreen)
+                    drawBlock(menuScreen, ds)
+                    modes["Home"] = False
+                    modes["Play"] = True
+                    updatediem(0)
+                    getbest()
+                    if game_over:
+                        drawgameover(menuScreen)
+                        musicHome.stop()
+                        modes["Play"] = False
+
+                if WIDTH / 2 - 125 <= mouse[0] <= (WIDTH / 2 - 125 + 250) and 370 <= mouse[1] <= 420 and modes[
+                    "Home"] == True:
+                    bangxephang(menuScreen)
+                    modes["Home"] = False
+                if WIDTH / 2 - 125 <= mouse[0] <= (WIDTH / 2 - 125 + 250) and 440 <= mouse[1] <= 490 and modes[
+                    "Home"] == True:
+                    running=False
+                if 22 <= mouse[0] <= 66 and 12 <= mouse[1] <= 52 and modes["Home"] == False:
+                    if game_over:
+                        musicHome.play()
+                    modes["Play"] = False
+                    setMenuScreen(menuScreen)
+                    modes["Home"] = True
+
+                if 69 <= mouse[0] <= 109 and 10 <= mouse[1] <= 50 and modes["Play"] == True:
+                    if game_over:
+                        musicHome.play()
+                        game_over = False
+                    souce = 0
+                    khoitao(ds)
+                    updatediem(0)
+                    getbest()
+                    background(menuScreen)
+                    drawBlock(menuScreen, ds)
+                    pygame.display.update()
+            if event.type == pygame.KEYDOWN and modes["Play"]== True:
+                dichuyen = False
+                if event.key == pygame.K_LEFT:
+                    dichuyen = dichtrai(ds)
+                elif event.key == pygame.K_RIGHT:
+                    dichuyen = dichphai(ds)
+                elif event.key == pygame.K_UP:
+                    dichuyen = dichlen(ds)
+                elif event.key == pygame.K_DOWN:
+                    dichuyen = dichxuong(ds)
+                if not isFull(ds) and dichuyen == True:
+                    add_khoi(ds)
+                if isWin(ds) and win == False:
+                    musicWin.play()
+                    win = True
+
+                output_ds(ds)
+                drawmh(menuScreen, ds)
+
+                if khongTheDiChuyen(ds) and dichuyen == True:
+                    musicHome.stop()
+                    musicGameOver.play()
+                    game_over = True
+                    modes["Play"] = False
+                    drawgameover(menuScreen)
+                    updateBXH(souce)
+
+            else:
+                continue
+        mouse = pygame.mouse.get_pos()
+        pygame.display.update()
